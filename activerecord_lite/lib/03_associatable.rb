1d98db9ed97ca27ee41b5b45bdf8e22cca2cc605
Ash Todd <ashztodd@gmail.com>
2019-07-30 17:00:43 -0700

    End of day

--- activerecord_lite/lib/03_associatable.rb
+++ activerecord_lite/lib/03_associatable.rb
@@ -10,34 +10,46 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+      @foreign_key = (options[:foreign_key] || "#{name.to_s.singularize}_id".to_sym)
+      @primary_key = (options[:primary_key] || :id)
+      @class_name = (options[:class_name] || name.to_s.camelcase.singularize )
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = (options[:foreign_key] || "#{self_class_name.to_s.underscore.singularize}_id".to_sym)
+    @primary_key = (options[:primary_key] || :id)
+    @class_name = (options[:class_name] || name.to_s.camelcase.singularize )
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      fval = self.send(options.foreign_key)
+      options.model_class.where({id: fval}).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, options)
+    define_method(name) do
+      pval = self.send(options.primary_key)
+      options.model_class.where()
+    end
   end
 
   def assoc_options
@@ -47,4 +59,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
